5bde66e155bdf8f3c0b406f8317e912991506b61
taitan <dachengfujing41@gmail.com>
2025-12-31 01:15:48 +0000

    configuration.nix の更新

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,6 +96,7 @@
 
 		#develpment
 		git
+　wget
 
 		##editor
 		neovim
@@ -141,19 +142,7 @@
   # services.pipewire = {
   #   enable = true;
   #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  #};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
