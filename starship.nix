4473ed37862b25f79d2655dcbadc35fc7cb0f1b8
TaiTan291 <dachnegfujing41@gmail.com>
2026-01-11 18:00:59 +0900

    Add starship.nix

--- starship.nix
+++ starship.nix
@@ -1,18 +1,38 @@
-{ pkgs, ... } :
+{ pkgs, ... } : 
 {
 	programs.starship = {
 		enable = true;
 		settings = {
 			add_newline = true;
-			command_timeout = 1300;
-			scan_timeout = 50;
 			format = ''
-				$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory
-			'';
+$directory[](fg:#99d1db bg:#eebebe)$direnv[](fg:#eebebe bg:#f4b8e4)$git_branch$git_status[](fg:#f4b8e4 bg:#babbf1)$cmd_duration[](fg:#babbf1)
+[│](bold green)$all$character'';
+			directory = {
+				format = "[ $path]($style)[ $read_only]($read_only_style)";
+				style = "bold #737994 bg:#99d1db";
+				read_only_style = "bold #e78284 bg:#99d1db";
+			};
+			direnv = {
+				disabled = false;
+				format = "[ $symbol$allowed]($style)";
+				style = "bold #838ba7 bg:#eebebe";
+			};
+			git_branch = {
+				format = "[ $symbol$branch(:$remote_branch)]($style)";
+				style = "bold #838ba7 bg:#f4b8e4";
+			};
+			git_status = {
+				format = "[ $all_status]($style)";
+				style = "bold #8caaee bg:#f4b8e4";
+			};
+			cmd_duration = {
+				format = "[  $duration]($style)";
+				style = "bold #838ba7 bg:#babbf1";
+			};
 			character = {
-				success_symbol = "[](bold green) ";
-			  error_symbol = "[✗](bold red) ";
+				success_symbol = "[└──>>](bold green)";
+				error_symbol = "[└──>](bold #e78284)";
 			};
 		};
-	};	
+	};
 }
