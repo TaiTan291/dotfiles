d72b70da886b042a29f65b119a257a699a0d901d
TaiTan291 <dachnegfujing41@gmail.com>
2026-01-11 20:31:48 +0900

    fix starship.nix

--- starship.nix
+++ starship.nix
@@ -1,38 +1,42 @@
-{ pkgs, ... } : 
+{ pkgs, ... }: 
 {
-	programs.starship = {
-		enable = true;
-		settings = {
-			add_newline = true;
-			format = ''
-$directory[](fg:#99d1db bg:#eebebe)$direnv[](fg:#eebebe bg:#f4b8e4)$git_branch$git_status[](fg:#f4b8e4 bg:#babbf1)$cmd_duration[](fg:#babbf1)
-[│](bold green)$all$character'';
-			directory = {
-				format = "[ $path]($style)[ $read_only]($read_only_style)";
-				style = "bold #737994 bg:#99d1db";
-				read_only_style = "bold #e78284 bg:#99d1db";
-			};
-			direnv = {
-				disabled = false;
-				format = "[ $symbol$allowed]($style)";
-				style = "bold #838ba7 bg:#eebebe";
-			};
-			git_branch = {
-				format = "[ $symbol$branch(:$remote_branch)]($style)";
-				style = "bold #838ba7 bg:#f4b8e4";
-			};
-			git_status = {
-				format = "[ $all_status]($style)";
-				style = "bold #8caaee bg:#f4b8e4";
-			};
-			cmd_duration = {
-				format = "[  $duration]($style)";
-				style = "bold #838ba7 bg:#babbf1";
-			};
-			character = {
-				success_symbol = "[└──>>](bold green)";
-				error_symbol = "[└──>](bold #e78284)";
-			};
-		};
-	};
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "\\[$username[@](bold green)$hostname[ ](bold green)$directory\\]$git_branch$direnv $cmd_duration
+[\\$ ](bold green)";
+      
+      username = {
+        format = "[$user](bold green)";
+        show_always = true;
+      };
+      
+      hostname = {
+        format = "[$hostname](bold green)";
+        ssh_only = false;
+      };
+      
+      directory = {
+        format = "[$path](bold green)[$read_only](bold red)";
+        truncation_length = 3;
+        truncate_to_repo = false;
+      };
+      
+      git_branch = {
+        format = " [\\(($branch)\\)](bold green)";
+        only_attached = false;
+      };
+      
+      direnv = {
+        disabled = false;
+        format = "[$symbol$allowed](bold green)";
+      };
+      
+      cmd_duration = {
+        min_time = 1000;
+        format = "[$duration](bold green)";
+      };
+    };
+  };
 }
