01e3b5f478cf7e5a45405a7fc4faebb398e6e56e
TaiTan291 <dachnegfujing41@gmail.com>
2026-01-13 08:52:25 +0900

    first commit

--- fcitx5/default.nix
+++ fcitx5/default.nix
@@ -1,6 +1,9 @@
-xdg.configFile = {
-	"fcitx5/profile".source = ./profile;
-	"fcitx5/config".source = ./config;
-	"fcitx5/conf/notifications.conf".source = ./conf/notifications.conf;
-	"fcitx5/conf/skk.conf".source = ./conf/skk.conf;
-};
+{ pkgs, config, ... }:
+{
+	xdg.configFile = {
+		"fcitx5/profile".source = ./profile;
+		"fcitx5/config".source = ./config;
+		"fcitx5/conf/notifications.conf".source = ./conf/notifications.conf;
+		"fcitx5/conf/skk.conf".source = ./conf/skk.conf;
+	};
+}
