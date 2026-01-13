4bb92b95cd34f98f5f2efcd4acb9cd318c59777e
TaiTan291 <dachnegfujing41@gmail.com>
2026-01-13 17:17:18 +0900

    first commit

--- nvim/default.nix
+++ nvim/default.nix
@@ -6,7 +6,7 @@ let
       owner = "vim-skk";
       repo = "skkeleton";
 			ver = 2.0.0;
-			sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+			sha256 = "954f2f96e74a0c409f12315278fb1bbef0286b60954f2f96e74a0c409f12315278fb1bbef0286b60";
 		};
   };
 
