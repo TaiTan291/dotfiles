1facf3f1602b9ab9e3dfda89ef6a32e354b8c22a
TaiTan291 <dachnegfujing41@gmail.com>
2026-01-13 10:59:11 +0900

    fix fcitx5

--- fcitx5/default.nix
+++ fcitx5/default.nix
@@ -1,9 +1,9 @@
 { pkgs, config, ... }:
 {
 	xdg.configFile = {
-		"fcitx5/profile".source = ./profile;
-		"fcitx5/config".source = ./config;
+		#"fcitx5/profile".source = ./profile;
+		#"fcitx5/config".source = ./config;
 		"fcitx5/conf/notifications.conf".source = ./conf/notifications.conf;
-		"fcitx5/conf/skk.conf".source = ./conf/skk.conf;
+		#"fcitx5/conf/skk.conf".source = ./conf/skk.conf;
 	};
 }
