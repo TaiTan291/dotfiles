d6b00458ecde9215061b71ed0152fb9f4aa1a323
TaiTan291 <dachnegfujing41@gmail.com>
2026-06-17 11:20:21 +0900

    fix warning firefox

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -1,8 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, config, lib, ... }: {
   programs.firefox = {
     enable = true;
+		configPath = "${config.xdg.configHome}/mozilla/firefox";
 		nativeMessagingHosts = [
 			pkgs.firefoxpwa
 		];
+  };
+	home.activation = {
+    # 既存のディレクトリが存在する場合のみ、XDGパスへ移行を実行
+    migrateFirefoxConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [ -d "$HOME/.mozilla/firefox" ]; then
+        $DRY_RUN_CMD mkdir -p "''${XDG_CONFIG_HOME:-$HOME/.config}/mozilla"
+        $DRY_RUN_CMD mv "$HOME/.mozilla/firefox" "''${XDG_CONFIG_HOME:-$HOME/.config}/mozilla/firefox"
+        $DRY_RUN_CMD rmdir "$HOME/.mozilla" 2>/dev/null || true
+      fi
+    '';
   };
 }
--- modules/nvim/plugins.nix
+++ modules/nvim/plugins.nix
@@ -70,11 +70,22 @@
           };
         };
 				postConfig = ''
-					require('lspconfig').ells.setup({
-						cmd = { "elisp-ls", "--stdio" },
-					filetypes = { "elisp" },
-				})
-			'';
+				local lspconfig = require('lspconfig')
+				local configs = require('lspconfig.configs')
+
+				if not configs.ells then
+					configs.ells = {
+						default_config = {
+							cmd = { "elisp-ls", "--stdio" },
+							filetypes = { "elisp" },
+							root_dir = lspconfig.util.root_pattern(".git", "."),
+							settings = {},
+						},
+					}
+				end
+
+				lspconfig.ells.setup({})
+				'';
       };
     };
   };
