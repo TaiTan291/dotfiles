a7b30c2f65aa4e9661dda93889127adc3532674a
TaiTan291 <dachnegfujing41@gmail.com>
2026-06-27 10:47:01 +0900

    feat: typst mode

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -8,22 +8,15 @@ in {
     enable = true;
     extraPackages = emacsPackages: with emacsPackages; [
       catppuccin-theme
+			typst-ts-mode
     ];
   };
 	xdg.configFile = {
     "emacs/conf".source = ./conf;
     "emacs/init.el".source = ./init.el;
   };
-  home = {
-    # packages = with pkgs; [
-    #   #typst
-    #   typst
-    #   tinymist
-    #   typstyle
-    #
-    #   cmake
-    #   libtool
-    #   libvterm-neovim
-    # ];
-  };
+  home.packages = with pkgs; [
+		typst
+		tinymist
+	];
 }
--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "/home/taitan/.config/emacs/conf")
 
 (load "ui")
-; (load "lang")
+(load "lang")
+(load "base")
