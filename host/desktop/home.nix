cf77e4b03c4a03a7c504686450a4c57b6a2ef48a
TaiTan291 <dachnegfujing41@gmail.com>
2026-06-27 21:30:50 +0900

    feat: rebuild

--- host/desktop/home.nix
+++ host/desktop/home.nix
@@ -1,4 +1,7 @@
-{...}: {
+{...}:let
+  home = ../../modules;
+in  {
   imports = [
+		(home + "/bash-desktop.nix")
   ];
 }
---
+++ modules/bash-desktop.nix
@@ -0,0 +1,7 @@
+{...}: {
+  programs.bash = {
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake /home/taitan/.config/nixos/.#desktop";
+    };
+  };
+}
